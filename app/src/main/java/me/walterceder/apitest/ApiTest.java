6d80163983ffff21b1de9510ce63655c92da5850
iGuest <fdf@fd.com>
2015-05-13 17:19:05 -0700

    readme

--- app/src/main/java/me/walterceder/apitest/ApiTest.java
+++ app/src/main/java/me/walterceder/apitest/ApiTest.java
@@ -1,7 +1,19 @@
 package me.walterceder.apitest;
 
+import android.app.Application;
+import android.util.Log;
+
 /**
  * Created by iguest on 5/13/15.
  */
-public class ApiTest {
+public class ApiTest extends Application{
+    private static ApiTest instance; // singleton
+
+    public ApiTest() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Log.e("ApiTest", "There is an error beep boop. You tried to create more than 1 the app");
+        }
+    }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Restaurantalations
+
+This is an Android App that grabs the health inspection data for local restaurants around you. Only works in King County, Washington, USA
