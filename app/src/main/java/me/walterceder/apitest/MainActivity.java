74869fc3952ca88cc5a32b8b393e9701b8b61b3b
iGuest <sdf@fd.com>
2015-05-21 20:35:54 -0700

    fixed idstance i hope

--- app/src/main/java/me/walterceder/apitest/MainActivity.java
+++ app/src/main/java/me/walterceder/apitest/MainActivity.java
@@ -34,7 +34,7 @@ package me.walterceder.apitest;
         import android.widget.TextView;
         import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener, LocationListener {
+public class MainActivity extends Activity implements LocationListener {
 
     private TextView latituteField;
     private TextView longitudeField;
@@ -55,58 +55,21 @@ public class MainActivity extends Activity implements OnClickListener, LocationL
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        intent = new Intent(this,SelectPlace.class);
-       // findViewById(R.id.button).setOnClickListener(this);
-       // latituteField = (TextView) findViewById(R.id.latituteField);
-      //  longitudeField = (TextView) findViewById(R.id.longitudeField);
-         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-
-
-
-        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-// check if enabled and if not send user to the GSP settings
-// Better solution would be to display a dialog and suggesting to
-// go to the settings
-        if (!enabled) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        // Initialize the locationObj fields
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-        } else {
-       //     latituteField.setText("Location not available");
-       //     longitudeField.setText("Location not available");
-        }
-        new LongRunningGetIO().execute();
+      // getPlaces();
     }
 
-    @Override
 
-    public void onClick(View arg0) {
-
-    //    Button b = (Button)findViewById(R.id.button);
-
-      //  b.setClickable(false);
-     //   b.setText("Loading");
-
-
-       //     new LongRunningGetIO().execute();
-
-
-
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
+        getPlaces();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+     //   getPlaces();
     }
 
     @Override
@@ -139,6 +102,39 @@ public class MainActivity extends Activity implements OnClickListener, LocationL
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
 
+    }
+
+    public void getPlaces(){
+        intent = new Intent(this,SelectPlace.class);
+        // findViewById(R.id.button).setOnClickListener(this);
+        // latituteField = (TextView) findViewById(R.id.latituteField);
+        //  longitudeField = (TextView) findViewById(R.id.longitudeField);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+
+
+
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+// check if enabled and if not send user to the GSP settings
+// Better solution would be to display a dialog and suggesting to
+// go to the settings
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the locationObj fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            //     latituteField.setText("Location not available");
+            //     longitudeField.setText("Location not available");
+        }
+        new LongRunningGetIO().execute();
     }
     private class LongRunningGetIO extends AsyncTask <Void, Void, String> {
         protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
@@ -256,7 +252,10 @@ public class MainActivity extends Activity implements OnClickListener, LocationL
                                     curr.addType(oneObject.getString("violation_type"));
                                     double tempLat = oneObject.getDouble("latitude");
                                     double tempLng = oneObject.getDouble("longitude");
-                                    curr.addDistance(Math.sqrt(Math.pow(lat-tempLat,2)+Math.pow(lng-tempLng,2))/0.000008998719243599958);
+                                    //old distance calculation
+                                    //curr.addDistance(Math.sqrt(Math.pow(lat-tempLat,2)+Math.pow(lng-tempLng,2))/0.000008998719243599958);
+                                    //new distance calculation
+                                    curr.addDistance(haversine(lat,lng,tempLat,tempLng));
 
                                 }
 
@@ -303,6 +302,53 @@ public class MainActivity extends Activity implements OnClickListener, LocationL
           // b.setClickable(true);
 
         }
+
+        /**
+         * Calculates the distance in km between two lat/long points
+         * using the haversine formula
+         */
+        public  double haversine(
+                double lat1, double lng1, double lat2, double lng2) {
+            int r = 6371; // average radius of the earth in km
+            double dLat = Math.toRadians(lat2 - lat1);
+            double dLon = Math.toRadians(lng2 - lng1);
+            double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                            * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            double d = r * c;
+            Log.i("Distance",d+"");
+            //convert ot m
+            return d*1000.0;
+        }
+
+        private double distance(double lat1, double lon1, double lat2, double lon2) {
+              double theta = lon1 - lon2;
+              double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+              dist = Math.acos(dist);
+            	  dist = rad2deg(dist);
+            	  dist = dist * 60 * 1.1515;
+            	  //convert to KM
+                    dist = dist * 1.609344;
+                    //convert to m
+                    dist = dist/1000.0;
+                    Log.i("Distance",dist+"");
+            	  return (dist);
+            	}
+
+                	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+                	/*::  This function converts decimal degrees to radians             :*/
+                	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+                private double deg2rad(double deg) {
+            	  return (deg * Math.PI / 180.0);
+            	}
+
+                	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+                	/*::  This function converts radians to decimal degrees             :*/
+                	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+                private double rad2deg(double rad) {
+            	  return (rad * 180 / Math.PI);
+            	}
 
     }
 }
